bad615ec738b6a0c0e998ef0e4f2569f0b33232b
Zach Reedy <paraton@hotmail.com>
2013-02-14 23:17:15 -0500

    Lexer cleaned
    Fixed a few more issues.
    Added support for alternate string syntax.

--- Source/Lexer/Tokenizer.cs
+++ Source/Lexer/Tokenizer.cs
@@ -16,11 +16,16 @@ namespace Xi.Lexer
 			"~", "!", "%", "^", "&", "*", "(", ")", "-", "+", "=", "[", "]", "{", "}", "|", ":", ";", "?", "/", "<", ",", ">", "."
 		};
 
+		private static string filename;
+		private static uint line;
+
 		public static List<Token> ParseString(string value)
 		{
+			filename = "";
+
 			try
 			{
-				return ParseStream(new StringStream(value), "");
+				return ParseStream(new StringStream(value));
 			}
 			catch (Exception e)
 			{
@@ -29,164 +34,53 @@ namespace Xi.Lexer
 			}
 		}
 
-		public static List<Token> ParseFile(string filename)
+		public static List<Token> ParseFile(string fname)
 		{
+			filename = fname;
+
 			try
 			{
-				using (StreamReader reader = new StreamReader(filename))
+				using (StreamReader reader = new StreamReader(fname))
 				{
-					return ParseStream(new StringStream(reader.ReadToEnd()), filename);
+					return ParseStream(new StringStream(reader.ReadToEnd()));
 				}
 			}
-			catch(IOException)
+			catch (IOException)
 			{
 				Console.WriteLine("Error: Cannot open file \"{0}\" for reading.", filename);
 				return null;
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
 				Console.WriteLine("Lexer Error: {0}", e.Message);
 				return null;
 			}
 		}
 
-		private static List<Token> ParseStream(StringStream stream, string filename)
+		private static List<Token> ParseStream(StringStream stream)
 		{
 			List<Token> tokens = new List<Token>();
-			uint line = 1;
+			line = 1;
 
 			while (!stream.IsEndOfStream)
 			{
-				// Skip whitespace
-				while (!stream.IsEndOfStream && char.IsWhiteSpace(stream.Peek()))
-				{
-					// Read new lines
-					if (stream.Read() == StringStream.NewLine)
-						++line;
-				}
+				SkipWhitespace(stream);
 
-				// Skip single line comments
-				if (stream.Peek() == '/' && stream.PeekAhead(1) == '/')
-				{
-					while (!stream.IsEndOfStream && stream.Peek() != StringStream.NewLine)
-						stream.Read();
-
+				if (SkipComment(stream))
 					continue;
-				}
 
-				// Skip multi line comments
-				if (stream.Peek() == '/' && stream.PeekAhead(1) == '*')
-				{
-					while (!stream.IsEndOfStream && !(stream.Peek() == '*' && stream.PeekAhead(1) == '/'))
-					{
-						if (stream.Read() == StringStream.NewLine)
-							++line;
-					}
-
-					stream.Position += 2;
+				if (ParseString(stream, tokens))
 					continue;
-				}
 
-				// TODO Proper multiline strings
-				// Parse strings
-				if (stream.Peek() == '\"')
-				{
-					uint startLine = line;
-					string value = "";
-					char ch;
-
-					++stream.Position;
-					while ((ch = stream.Read()) != '\"')
-					{
-						if (stream.IsEndOfStream)
-							throw new Exception("Unterminated string on line " + startLine);
-
-						if (ch == StringStream.NewLine)
-							++line;
-
-						value += ch;
-					}
-
-					tokens.Add(new Token(TokenType.String, value, filename, startLine));
+				if (ParseWord(stream, tokens))
 					continue;
-				}
 
-				// Parse words
-				if (char.IsLetter(stream.Peek()) || stream.Peek() == '_')
-				{
-					string word = "";
-
-					while (!stream.IsEndOfStream && (char.IsLetterOrDigit(stream.Peek()) || stream.Peek() == '_'))
-						word += stream.Read();
-
-					tokens.Add(new Token(TokenType.Word, word, filename, line));
+				if (ParseNumber(stream, tokens))
 					continue;
-				}
 
-				// Parse numbers
-				if (char.IsDigit(stream.Peek()) || (stream.Peek() == '-' && char.IsDigit(stream.PeekAhead(1))))
-				{
-					string number = "";
-					if (stream.Peek() == '-')
-						number += stream.Read();
-
-					if (stream.Peek() == '0' && stream.PeekAhead(1) == 'x')
-					{
-						stream.Position += 2;
-
-						while (!stream.IsEndOfStream && char.IsLetterOrDigit(stream.Peek()))
-							number += stream.Read();
-
-						ulong result;
-						try
-						{
-							result = ulong.Parse(number, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-						}
-						catch (OverflowException)
-						{
-							throw new Exception("Number is larger than 64bits on line " + line);
-						}
-						catch (Exception)
-						{
-							throw new Exception("Invalid number on line " + line);
-						}
-
-						number = result.ToString("G");
-					}
-					else
-					{
-						while (!stream.IsEndOfStream && (char.IsDigit(stream.Peek()) || stream.Peek() == '.'))
-							number += stream.Read();
-					}
-
-					tokens.Add(new Token(TokenType.Number, number, filename, line));
+				if (ParseDelimiter(stream, tokens))
 					continue;
-				}
 
-				// Parse delimiters
-				bool foundDelimiter = false;
-				string peekedDelimiter = "";
-				foreach (string del in delimiters)
-				{
-					peekedDelimiter = "";
-					for (int i = 0; i < del.Length; ++i)
-						peekedDelimiter += stream.PeekAhead(i);
-
-					if (peekedDelimiter == del)
-					{
-						stream.Position += del.Length;
-						foundDelimiter = true;
-						break;
-					}
-				}
-
-				if (foundDelimiter)
-				{
-					tokens.Add(new Token(TokenType.Delimiter, peekedDelimiter, filename, line));
-					continue;
-				}
-
-				// Don't run off the rails
 				if (stream.IsEndOfStream)
 					break;
 
@@ -196,6 +90,162 @@ namespace Xi.Lexer
 			tokens.Add(new Token(TokenType.EndOfStream, filename, line));
 
 			return tokens;
+		}
+
+		private static void SkipWhitespace(StringStream stream)
+		{
+			while (!stream.IsEndOfStream && char.IsWhiteSpace(stream.Peek()))
+			{
+				// Read new lines
+				if (stream.Read() == StringStream.NewLine)
+					++line;
+			}
+		}
+
+		private static bool SkipComment(StringStream stream)
+		{
+			// Skip single line comments
+			if (stream.Peek() == '/' && stream.PeekAhead(1) == '/')
+			{
+				while (!stream.IsEndOfStream && stream.Peek() != StringStream.NewLine)
+					stream.Read();
+
+				return true;
+			}
+
+			// Skip multi line comments
+			if (stream.Peek() == '/' && stream.PeekAhead(1) == '*')
+			{
+				while (!stream.IsEndOfStream && !(stream.Peek() == '*' && stream.PeekAhead(1) == '/'))
+				{
+					if (stream.Read() == StringStream.NewLine)
+						++line;
+				}
+
+				stream.Position += 2;
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool ParseString(StringStream stream, List<Token> tokens)
+		{
+			if (stream.Peek() != '\"')
+				return false;
+
+			uint startLine = line;
+			string value = "";
+
+			while (stream.Peek() == '\"')
+			{
+				char ch;
+
+				++stream.Position;
+				while ((ch = stream.Read()) != '\"')
+				{
+					if (stream.IsEndOfStream)
+						throw new Exception("Unterminated string on line " + startLine);
+
+					if (ch == StringStream.NewLine)
+						++line;
+
+					value += ch;
+				}
+
+				SkipWhitespace(stream);
+			}
+
+			tokens.Add(new Token(TokenType.String, value, filename, startLine));
+
+			return true;
+		}
+
+		private static bool ParseWord(StringStream stream, List<Token> tokens)
+		{
+			if (char.IsLetter(stream.Peek()) || stream.Peek() == '_')
+			{
+				string word = "";
+
+				while (!stream.IsEndOfStream && (char.IsLetterOrDigit(stream.Peek()) || stream.Peek() == '_'))
+					word += stream.Read();
+
+				tokens.Add(new Token(TokenType.Word, word, filename, line));
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool ParseNumber(StringStream stream, List<Token> tokens)
+		{
+			if (char.IsDigit(stream.Peek()) || (stream.Peek() == '-' && char.IsDigit(stream.PeekAhead(1))))
+			{
+				string number = "";
+				if (stream.Peek() == '-')
+					number += stream.Read();
+
+				if (stream.Peek() == '0' && stream.PeekAhead(1) == 'x')
+				{
+					stream.Position += 2;
+
+					while (!stream.IsEndOfStream && char.IsLetterOrDigit(stream.Peek()))
+						number += stream.Read();
+
+					ulong result;
+					try
+					{
+						result = ulong.Parse(number, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+					}
+					catch (OverflowException)
+					{
+						throw new Exception("Number is larger than 64bits on line " + line);
+					}
+					catch (Exception)
+					{
+						throw new Exception("Invalid number on line " + line);
+					}
+
+					number = result.ToString("G");
+				}
+				else
+				{
+					while (!stream.IsEndOfStream && (char.IsDigit(stream.Peek()) || stream.Peek() == '.'))
+						number += stream.Read();
+				}
+
+				tokens.Add(new Token(TokenType.Number, number, filename, line));
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool ParseDelimiter(StringStream stream, List<Token> tokens)
+		{
+			bool foundDelimiter = false;
+			string peekedDelimiter = "";
+			foreach (string del in delimiters)
+			{
+				peekedDelimiter = "";
+				for (int i = 0; i < del.Length; ++i)
+					peekedDelimiter += stream.PeekAhead(i);
+
+				if (peekedDelimiter == del)
+				{
+					stream.Position += del.Length;
+					foundDelimiter = true;
+					break;
+				}
+			}
+
+			if (foundDelimiter)
+			{
+				tokens.Add(new Token(TokenType.Delimiter, peekedDelimiter, filename, line));
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
